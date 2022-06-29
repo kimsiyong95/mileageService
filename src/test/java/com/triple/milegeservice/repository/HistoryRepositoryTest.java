c75dc0e8938f5cf88b4d7aa57b8437abec7a107f
kimsiyong95 <kimsiyong95@gmail.com>
2022-06-29 23:08:48 +0900

    test : 마일리지 서비스 테스트 코드 작성중, 히스토리 레포 테스트 코드 작성

--- src/test/java/com/triple/milegeservice/repository/HistoryRepositoryTest.java
+++ src/test/java/com/triple/milegeservice/repository/HistoryRepositoryTest.java
@@ -1,11 +1,16 @@
 package com.triple.milegeservice.repository;
 
 import com.triple.milegeservice.config.QueryDslConfigTest;
+import com.triple.milegeservice.domain.History;
+import com.triple.milegeservice.domain.Review;
+import com.triple.milegeservice.domain.common.RequestDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,5 +25,26 @@ class HistoryRepositoryTest {
     @Test
     public void historyRepositoryIsNotNull(){
         assertThat(historyRepository).isNotNull();
+    }
+
+    @DisplayName("히스토리 저장 테스트")
+    @Test
+    public void addHistoryTest(){
+
+        RequestDTO requestDTO = new RequestDTO();
+
+        Review review = Review.builder().reviewId(UUID.randomUUID().toString())
+                                       .content("내용")
+                                       .userId(UUID.randomUUID().toString())
+                                       .placeId(UUID.randomUUID().toString())
+                                       .deleteYn("N")
+                                       .build();
+
+        History createHistory = History.createHistory(requestDTO, 10, 3, review);
+
+        History saveHistory = historyRepository.save(createHistory);
+
+        assertThat(saveHistory).isNotNull();
+        assertThat(saveHistory.getTotPoint()).isEqualTo(13);
     }
 }
---
+++ src/test/java/com/triple/milegeservice/service/MileageServiceTest.java
@@ -0,0 +1,115 @@
+package com.triple.milegeservice.service;
+
+import com.triple.milegeservice.domain.History;
+import com.triple.milegeservice.domain.Review;
+import com.triple.milegeservice.domain.UserPoint;
+import com.triple.milegeservice.domain.common.RequestDTO;
+import com.triple.milegeservice.domain.common.ResponseDTO;
+import com.triple.milegeservice.repository.HistoryRepository;
+import com.triple.milegeservice.repository.ReviewRepository;
+import com.triple.milegeservice.repository.UserPointRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class MileageServiceTest {
+
+    @Mock
+    private ReviewRepository reviewRepository;
+
+    @Mock
+    private UserPointRepository userPointRepository;
+
+    @Mock
+    private HistoryRepository historyRepository;
+
+    @InjectMocks
+    private MileageService mileageService;
+
+
+
+
+
+
+    @DisplayName("리뷰등록 및 포인트 부여 로직 테스트")
+    @Test
+    public void addReview(){
+        List<String> photos = new ArrayList<>();
+        photos.add(UUID.randomUUID().toString());
+
+        RequestDTO requestDTO = makeRequestDto("ADD", photos);
+
+        int point = mileageService.createPoint(requestDTO);
+        Review saveReview = Review.createReview(requestDTO, point);
+        UserPoint userPoint = UserPoint.createUserPoint(requestDTO);
+        History createHistory = History.createHistory(requestDTO, userPoint.getPoint(), point, saveReview);
+
+        when(reviewRepository.findByUserIdAndPlaceId(requestDTO)).thenReturn(null);
+        when(reviewRepository.findPlaceCount(requestDTO)).thenReturn(0L);
+        when(reviewRepository.save(any())).thenReturn(saveReview);
+        when(userPointRepository.findById(requestDTO.getUserId())).thenReturn(Optional.ofNullable(userPoint));
+        when(historyRepository.save(any())).thenReturn(createHistory);
+
+        ResponseDTO responseDTO = mileageService.addReview(requestDTO);
+
+        assertThat(responseDTO).isNotNull();
+        assertThat(responseDTO.getCode()).isEqualTo(201);
+        assertThat(responseDTO.getMessage()).isEqualTo("CREATED");
+    }
+
+
+    @DisplayName("리뷰 수정 및 포인트 부여 로직 테스트")
+    @Test
+    public void modReview(){
+        RequestDTO requestDTO = makeRequestDto("MOD", new ArrayList<>());
+
+        List<String> photosId = new ArrayList<>();
+        photosId.add(UUID.randomUUID().toString());
+        RequestDTO updateDTO = makeRequestDto("MOD", photosId);
+        updateDTO.setUserId(requestDTO.getUserId());
+
+        Review findReview = Review.createReview(updateDTO, 3);
+        UserPoint userPoint = UserPoint.createUserPoint(requestDTO);
+        History createHistory = History.createHistory(requestDTO, userPoint.getPoint(), 3, findReview);
+
+        when(reviewRepository.findByIdAndDeleteYn(any())).thenReturn(Optional.ofNullable(findReview));
+        when(userPointRepository.findById(requestDTO.getUserId())).thenReturn(Optional.ofNullable(userPoint));
+        when(historyRepository.save(any())).thenReturn(createHistory);
+
+        ResponseDTO responseDTO = mileageService.modReview(requestDTO);
+
+        assertThat(responseDTO).isNotNull();
+        assertThat(responseDTO.getCode()).isEqualTo(201);
+        assertThat(responseDTO.getMessage()).isEqualTo("CREATED");
+    }
+
+
+    public RequestDTO makeRequestDto(String action, List<String> photos){
+        RequestDTO requestDTO = new RequestDTO();
+
+        requestDTO.setType("REVIEW");
+        requestDTO.setAction(action);
+        requestDTO.setReviewId(UUID.randomUUID().toString());
+        requestDTO.setContent("test");
+        requestDTO.setAttachedPhotoIds(photos);
+        requestDTO.setUserId(UUID.randomUUID().toString());
+        requestDTO.setPlaceId(UUID.randomUUID().toString());
+
+        return requestDTO;
+    }
+
+}
